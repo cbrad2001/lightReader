03cdb54f0bafd3ab8a393b234310627076fa9867
cbrad2001 <caleb_bradley@sfu.ca>
2023-01-26 23:11:43 -0800

    Added optional hint .h

---
+++ makefile
@@ -0,0 +1,15 @@
+OUTFILE = hello
+OUTDIR = $(HOME)/cmpt433/public/myApps
+
+CROSS_COMPILE = arm-linux-gnueabihf-
+CC_C = $(CROSS_COMPILE)gcc
+CFLAGS = -Wall -g -std=c99 -D _POSIX_C_SOURCE=200809L -Werror -Wshadow 
+
+app: 
+		$(CC_C) $(CFLAGS) noworky.c -o $(OUTDIR)/$(OUTFILE) 
+
+all: 
+		$(CC_C) $(CFLAGS) noworky.c -o $(OUTDIR)/$(OUTFILE) 
+		
+clean: 
+		rm $(OUTDIR)/$(OUTFILE)
---
+++ sampler.c
@@ -0,0 +1,45 @@
+
+
+
+
+void Sampler_startSampling(void)
+{
+
+}
+
+void Sampler_stopSampling(void)
+{
+
+}
+
+
+void Sampler_setHistorySize(int newSize)
+{
+
+}
+
+int Sampler_getHistorySize(void)
+{
+
+}
+
+double* Sampler_getHistory(int *length)
+{
+
+}
+
+int Sampler_getNumSamplesInHistory()
+{
+
+}
+
+
+double Sampler_getAverageReading(void)
+{
+
+}
+
+long long Sampler_getNumSamplesTaken(void)
+{
+    
+}
