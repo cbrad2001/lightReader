9eec2891760bb5e1b4ff678e76480bd3816f7e0c
Jasper Wong <lazypigz.mw3@gmail.com>
2023-01-29 22:39:56 -0800

    Added module for reading value from POT.

--- include/potentiometer.h
+++ include/potentiometer.h
@@ -11,10 +11,12 @@ It provides access to a getting a raw value reading from the POT.
 
 // Returns a the raw voltage value from the POT from 0-4095 inclusive. 
 // If it is unable to read the value, it returns -1.
+// Note that the POT value itself is only updated once per second in this program.
 int Pot_getRawValue(void);
 
 // Converts the raw value reading from the POT to an actual voltage value.
 // Returns -1 if there is an error reading the raw value of the POT.
+// Note that the POT value itself is only updated once per second in this program.
 double Pot_getVoltage(void);
 
 // Starts/ends the thread that reads the POT once per second in the background.
